e24f669a9c630e7cdff2d05f20c608d44646c41a
maxikbe <belunekmax@gmail.com>
2025-01-29 22:33:46 +0100

    small nav bar screen/menu fixed-working

--- javascript.js
+++ javascript.js
@@ -1,7 +1,19 @@
 function MenuOut(){
-    if(document.getElementById('Menu').classList.contains("close-icon-anim")){
-        document.getElementById('Menu').classList.replace("close-icon-anim","open-icon-anim");
-    }else{
-        document.getElementById('Menu').classList.remove("open-icon-anim","close-icon-anim");
+    if(document.getElementById('Menu').classList.contains("opened-nav")){
+        document.getElementById('Menu').classList.remove("play-open-anim");
+        document.getElementById('Menu').classList.add("play-close-anim");
+        setTimeout(function(){
+            document.getElementById('Menu').classList.remove("opened-nav");
+            document.getElementById('Menu').classList.add("closed-nav");
+            document.getElementById('Menu').classList.remove("play-close-anim");
+        }, 2000);
+    }else if(document.getElementById('Menu').classList.contains("closed-nav")){
+        document.getElementById('Menu').classList.remove("play-close-anim");
+        document.getElementById('Menu').classList.add("play-open-anim");
+        setTimeout(function(){
+            document.getElementById('Menu').classList.remove("closed-nav");
+            document.getElementById('Menu').classList.add("opened-nav");
+            document.getElementById('Menu').classList.remove("play-open-anim");
+        }, 2000);
     }
 }
