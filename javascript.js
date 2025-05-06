dc0941d4cb496590f5e8f60b3ffc19335cf0c0ea
maxikbe <belunekmax@gmail.com>
2025-05-06 10:09:54 +0200

    Website Klauzury

--- javascript.js
+++ javascript.js
@@ -1,19 +1,6 @@
-function MenuOut(){
-    if(document.getElementById('Menu').classList.contains("opened-nav")){
-        document.getElementById('Menu').classList.remove("play-open-anim");
-        document.getElementById('Menu').classList.add("play-close-anim");
-        setTimeout(function(){
-            document.getElementById('Menu').classList.remove("opened-nav");
-            document.getElementById('Menu').classList.add("closed-nav");
-            document.getElementById('Menu').classList.remove("play-close-anim");
-        }, 500);
-    }else if(document.getElementById('Menu').classList.contains("closed-nav")){
-        document.getElementById('Menu').classList.remove("play-close-anim");
-        document.getElementById('Menu').classList.add("play-open-anim");
-        setTimeout(function(){
-            document.getElementById('Menu').classList.remove("closed-nav");
-            document.getElementById('Menu').classList.add("opened-nav");
-            document.getElementById('Menu').classList.remove("play-open-anim");
-        }, 500);
-    }
+function otevrit(){
+    document.getElementById('sidebar').style.right = "0px";
+}
+function zavrit(){
+    document.getElementById('sidebar').style.right = "-100vw";
 }
